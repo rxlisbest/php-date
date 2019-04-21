11ac022964796e66208d6499778e493628f7698c
Roy <rxlisbest@gmail.com>
2019-04-21 22:15:40 +0800

    自动加载

---
+++ src/PhpDate.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2019-04-21
+ * Time: 22:10
+ */
+
+namespace Rxlisbest\PhpDate;
+
+class PhpDate
+{
+    private $date;
+
+    public function __construct($date)
+    {
+        $this->date = $date;
+        var_dump($data);
+    }
+}
